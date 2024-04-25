59c3196ad83611fde769bf8c3e250824d74c75d0
S.Gyanjoth Singh <S.Gyanjoth Singh@Gyan-Pavilion>
2024-04-25 12:31:29 +0530

    rydjr

--- gyan/src/gyan/studentinfo.java
+++ gyan/src/gyan/studentinfo.java
@@ -18,7 +18,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class studentinfo {
-
+//abvksvnbkjv
 	private JFrame frame;
 	private JTextField t1;
 	private JTextField t2;
